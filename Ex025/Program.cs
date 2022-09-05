90b2b759d07e1c03d2a194128c6e291a5cfa965d
Katya <katvas785@gmail.com>
2022-09-05 18:41:15 +0300

    Ex025

---
+++ Ex025/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int a =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int s=1;
+int i =0;
+while(i<b)
+{
+    s=s*a;
+    i++;
+}
+Console.WriteLine(s);
+
