7cc97426821b8703e0bb10e8e3c3094461998fe9
Katya <katvas785@gmail.com>
2022-09-05 19:03:14 +0300

    Ex000

---
+++ Ex000/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+int y = 0;
+while(y <8)
+{
+     array[y] = new Random().Next(0, 2);
+     Console.WriteLine(array[y]); 
+     y++;
+}
